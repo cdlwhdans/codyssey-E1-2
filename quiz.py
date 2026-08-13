d458d9be9f720db1e9167e1e4a21da2fe85e9d07
cdlwhdans <cdlwhdans98@gmail.com>
2026-08-13 13:12:59 +0900

    feat: Quiz 클래스 구현

---
+++ quiz.py
@@ -0,0 +1,14 @@
+class Quiz:
+    def __init__(self, question, choices, answer):
+        self.question = question
+        self.choices = choices
+        self.answer = answer
+
+    def display(self):
+        print(self.question)
+        print("")
+        for idx, choice in enumerate(self.choices):
+            print(f"{idx + 1}. {choice}")
+
+    def is_correct(self, user_answer):
+        return self.answer == user_answer
