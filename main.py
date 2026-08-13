5489f1f9d1f4327bf265422c63d6fcf35525004e
cdlwhdans <cdlwhdans98@gmail.com>
2026-08-13 11:19:37 +0900

    feat: QuizGame 기본 구조 및 실행 진입점 구현

---
+++ main.py
@@ -0,0 +1,11 @@
+from quiz_game import QuizGame
+
+def main():
+    try:
+        game = QuizGame()
+        game.run()
+    except (KeyboardInterrupt, EOFError):
+        print("\n입력이 중단되어 프로그램을 종료합니다.")
+    
+if __name__ == "__main__":
+    main()
---
+++ quiz_game.py
@@ -0,0 +1,28 @@
+class QuizGame:
+    def __init__(self):
+        self.quizzes= []
+        self.best_score = None
+
+    def run(self):
+        pass
+
+    def show_menu(self):
+        pass
+
+    def play_quiz(self):
+        pass
+
+    def add_quiz(self):
+        pass
+
+    def show_quizzes(self):
+        pass
+
+    def show_best_score(self):
+        pass
+
+    def save_state(self):
+        pass
+    
+    def load_state(self):
+        pass
